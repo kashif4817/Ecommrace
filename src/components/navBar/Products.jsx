e4ae2c7928a3119d8cde307f78872edddc55d05d
Kashif4817 <0112kashif@gmail.com>
2025-07-06 00:36:01 +0500

    ttest

--- src/components/navBar/Products.jsx
+++ src/components/navBar/Products.jsx
@@ -5,6 +5,7 @@ const Products = () => {
     <div className='flex items-center justify-center'
     >
       <h1 className='flex'>Products</h1>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum fuga porro officiis iste ut quod! Repellendus, excepturi adipisci molestiae architecto quos at labore deserunt ad possimus exercitationem omnis repellat incidunt.
     </div>
   )
 }
